2419697f2d346c4d7dcb73e978c9b3a6bb9f0554
Nicholas Junge <nicholas.junge@web.de>
2021-07-09 11:53:58 +0200

    Add setup.py prototype and rework requirements.txt
    This commit adds a fairly minimal setup.py and clears up additional
    requirements.
    
    The plan is to make pybm have as few requirements as necessary
    outside of the standard library to make it portable. This means that
    the benchmarking process will require installing one's benchmarking
    library of choice, or even write your own benchmarking class yourself.
    All in all, this seems like an acceptable tradeoff for the user, who
    gets a leaner package and more control, as long as there is a good
    default benchmark class to use out of the box, preferably by an
    optional pip dependency.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+def get_requirements() -> list[str]:
+    with open("requirements.txt", "r") as f:
+        reqs = f.readlines()
+    return reqs
+
+def get_version(fp) -> str:
+    with open(fp, "r") as f:
+        for line in f:
+            if "version" in line:
+                delim = "\""
+                return line.split(delim)[1]
+    raise RuntimeError(f"could not find a valid version string in {fp}.")
+
+
+setup(
+    name="pybm",
+    version=get_version("pybm/__init__.py"),
+    description="A Python CLI for reproducible benchmarking in a git "
+                "repository.",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+    entry_points={
+        'console_scripts': ['pybm=pybm.main:main']
+    },
+    author="Nicholas Junge",
+    license="MIT"
+)
