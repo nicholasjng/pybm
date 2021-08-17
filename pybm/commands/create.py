c159b3201595cd487627db0a83e64f831b6a14fe
Nicholas Junge <nicholas.junge@web.de>
2021-08-17 16:25:34 +0200

    Add commit resolving and verbose mode, make name argument optional
    This commit contains more command line options for `pybm create`.
    * -R, which resolves a given ref to a commit. This leads to detached
    HEADs for branches.
    * -v, verbose mode, which enables logging additional debug information.
    
    Additionally, the <dest> positional argument was made optional by
    enabling the `nargs="?"` argparse flag.

--- pybm/commands/create.py
+++ pybm/commands/create.py
@@ -4,7 +4,7 @@ from pybm.command import CLICommand
 from pybm.exceptions import GitError
 from pybm.git import GitWorktreeWrapper
 from pybm.git_utils import is_git_repository
-from pybm.status_codes import SUCCESS, ERROR
+from pybm.status_codes import SUCCESS
 
 
 class CreateCommand(CLICommand):
@@ -19,14 +19,14 @@ class CreateCommand(CLICommand):
     def add_arguments(self):
         self.parser.add_argument("commit-ish",
                                  metavar="<commit-ish>",
-                                 nargs=1,
                                  help="Commit, branch or tag to create a "
                                       "git worktree for.")
         self.parser.add_argument("dest",
                                  metavar="<dest>",
+                                 nargs="?",
                                  default=None,
-                                 help="Destination directory for the newly "
-                                      "created worktree. Defaults to "
+                                 help="Destination directory to create for "
+                                      "the new worktree. Defaults to "
                                       "repository-name@{commit|branch|tag}.")
         self.parser.add_argument("--python",
                                  type=str,
@@ -43,8 +43,20 @@ class CreateCommand(CLICommand):
                                  action="store_true",
                                  help="Skip worktree checkout after creation. "
                                       "This can be used to sparsely "
-                                      "check out branches where only a small "
-                                      "subset of code needs to be run.")
+                                      "check out branches.")
+        self.parser.add_argument("-R", "--resolve-commits",
+                                 action="store_true",
+                                 help="Always resolve the given git ref to "
+                                      "its associated commit. If the given "
+                                      "ref is a branch name, this detaches "
+                                      "the HEAD (see https://git-scm.com/docs/"
+                                      "git-checkout#_detached_head).")
+        self.parser.add_argument("-v",
+                                 action="count",
+                                 default=0,
+                                 help="Enable verbose mode. This causes pybm "
+                                      "to log information useful for "
+                                      "debugging.")
 
     def run(self, *args, **kwargs) -> int:
         self.add_arguments()
@@ -52,14 +64,20 @@ class CreateCommand(CLICommand):
         namespace = self.parser.parse_args(*args)
         var_dict = vars(namespace)
 
+        verbose = var_dict.pop("v")
+        if verbose:
+            print(var_dict)
+
         if not is_git_repository():
             raise GitError("No git repository present in this path")
 
         git = GitWorktreeWrapper()
 
         git.add_worktree(var_dict["commit-ish"],
-                         var_dict["dest"],
-                         force=var_dict["f"]
+                         name=var_dict["dest"],
+                         force=var_dict["force"],
+                         checkout=not var_dict["no_checkout"],
+                         resolve_commits=var_dict["resolve_commits"]
                          )
 
         return SUCCESS
