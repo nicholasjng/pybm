81e3e950da1167c1149bf7414dd7cfb24ac8d1a4
Nicholas Junge <nicholas.junge@web.de>
2021-08-17 16:21:56 +0200

    Refactor git worktree wrapper, add and remove implementations
    This commit adds more `git worktree` functionality, including first
    working implementations for `git worktree add` and `git worktree remove`.
    In addition, some getters by ref style and attribute for discovering
    remove candidates awre added.

--- pybm/git.py
+++ pybm/git.py
@@ -1,12 +1,20 @@
 """Small Git worktree wrapper for operating on a repository via Python."""
 
 import subprocess
-from typing import Optional, List, Dict, Union
+from pathlib import Path
+from typing import Optional, List, Dict
 from pybm.exceptions import GitError
-from pybm.git_utils import parse_flags, lmap, get_repository_name, list_tags, \
-    list_local_branches, resolve_commit, lfilter
+from pybm.git_utils import lmap, get_repository_name, \
+    list_local_branches, resolve_commit, lfilter, resolve_to_ref
 
-WORKTREE_ATTRS = ["worktree", "branch", "HEAD"]
+_git_worktree_flags = {
+    "add": {"force": {True: "-f", False: None},
+            "checkout": {True: "--checkout", False: "--no-checkout"},
+            "lock": {True: "--lock", False: None},
+            },
+    "list": {"porcelain": {True: "--porcelain", False: None}},
+    "remove": {"force": {True: "-f", False: None}}
+}
 
 
 class GitWorktreeWrapper:
@@ -16,85 +24,127 @@ class GitWorktreeWrapper:
         self.executable = "git"
         self.repository_name = get_repository_name()
 
+    @staticmethod
+    def parse_flags(command: str, **kwargs) -> List[str]:
+        flags = []
+        for k, v in kwargs.items():
+            command_options = _git_worktree_flags[command]
+            if k not in command_options:
+                # TODO: log bad kwarg usage somewhere
+                continue
+            cmd_opts = command_options[k]
+            if v not in cmd_opts:
+                raise ValueError(f"unknown value {v} given for option {k}.")
+            flag = cmd_opts[v]
+            if flag is not None:
+                flags.append(flag)
+
+        return flags
+
     def run_command(self, command: str, *args, **kwargs) -> str:
         call_args = [self.executable, "worktree", command]
 
         call_args.extend(list(args))
         # parse git command line args separately
-        call_args.extend(parse_flags(command, **kwargs))
+        call_args.extend(self.parse_flags(command, **kwargs))
         try:
             return subprocess.check_output(call_args).decode("utf-8")
         except subprocess.CalledProcessError as e:
             full_command = " ".join(call_args)
-            msg = f"The command `git {full_command}` returned the non-zero " \
+            msg = f"The command `{full_command}` returned the non-zero " \
                   f"exit code {e.returncode}. Further information (output of " \
-                  f"the subprocess):\n {e.output}"
+                  f"the subprocess command):\n\n {e.output.decode()}"
             raise GitError(msg)
 
-    def get_worktree_by_attr(self, attr: Union[str, List[str]], value: str):
+    def get_worktree_by_name(self, name: str):
         worktrees = self.list_worktrees()
-        attrs, wt = attr if isinstance(attr, str) else attr, None
-        for attr in attrs:
-            try:
-                # all values are strings, so partial matching can be employed
-                # with the `in` operator here
-                wt = next(wt for wt in worktrees if wt[attr] in value)
-            except StopIteration:
-                continue
+        try:
+            # absolute paths, match names against bottom level directories
+            wt = next(wt for wt in worktrees if wt["worktree"].endswith(name))
+        except StopIteration:
+            wt = None
         return wt
 
-    def list_worktrees(self) -> List[Dict[str, str]]:
-        attr_list = self.run_command("list", "--porcelain").splitlines()
+    def get_worktree_by_commit(self, commit_sha: str):
+        worktrees = self.list_worktrees()
+        try:
+            # partial matching commit SHA with `in`, returns first occurrence
+            wt = next(wt for wt in worktrees if commit_sha in wt["HEAD"])
+        except StopIteration:
+            wt = None
+        return wt
+
+    def get_worktree_by_branch(self, name: str):
+        worktrees = self.list_worktrees()
+        try:
+            # branches are in the form refs/heads/$BRANCH or "detached"
+            wt = next(wt for wt in worktrees if wt["branch"].split("/")[-1]
+                      == name)
+        except StopIteration:
+            wt = None
+        return wt
+
+    def get_worktree_by_ref_type(self, ref: str, ref_type: str):
+        handlers = {
+            "tag": lambda x: self.get_worktree_by_commit(resolve_commit(x)),
+            "branch": self.get_worktree_by_branch,
+            "commit": self.get_worktree_by_commit
+        }
+        return handlers.get(ref_type)(ref)
+
+    def list_worktrees(self, porcelain: bool = True) -> List[Dict[str, str]]:
+        attr_list = self.run_command("list", porcelain=porcelain).splitlines()
         # git worktree porcelain lists are twice newline-terminated at the end,
-        # creating an empty string when applying str.splitlines() - these are
-        # filtered out here
+        # creating an empty string when applying str.splitlines()
         attr_list = lfilter(lambda x: x != "", attr_list)
-        num_attrs = len(attr_list)
         # detached HEADs have no associated branch, prepend the attribute so
         # that we produce the same dictionary schema every time
         attr_list = lmap(
             lambda x: str.split("branch " + x if x == "detached" else x),
             attr_list)
         # worktree attributes are directory, HEAD and branch
-        attr_list = [attr_list[i:i+3] for i in range(0, num_attrs, 3)]
+        attr_list = [attr_list[i:i + 3] for i in range(0, len(attr_list), 3)]
         return lmap(dict, attr_list)
 
     def add_worktree(self, commit_ish: str, name: Optional[str] = None,
-                     force: bool = False, checkout: bool = False,
+                     force: bool = False, checkout: bool = True,
                      lock: bool = False, resolve_commits: bool = False):
 
-        if not force and self.get_worktree_by_attr(WORKTREE_ATTRS, commit_ish):
-            msg = f"fatal: Worktree with ID {commit_ish} already exists. If " \
-                  f"you want to check out the same ref multiple times, " \
-                  f"supply the -f/--force option to `pybm create`."
-            raise GitError(msg)
+        ref, ref_type = resolve_to_ref(commit_ish,
+                                       resolve_commits=resolve_commits)
 
-        ref = commit_ish
-        if resolve_commits:
-            # TODO: Resolving is ambiguous if there are branches and tags
-            #  with the same name. Figure this out
-            if commit_ish in list_tags():
-                # ref is a tag name
-                ref = resolve_commit(commit_ish, ref_type="tag")
-            elif commit_ish in list_local_branches():
-                # ref is a branch name, check it out at HEAD
-                ref = resolve_commit(commit_ish, ref_type="branch")
-            else:
-                pass
+        # check for existing worktree with the same spec
+        if not force and self.get_worktree_by_ref_type(ref, ref_type=ref_type):
+            msg = f"Worktree for {ref_type} {commit_ish} already exists. " \
+                  f"If you want to check out the same {ref_type} " \
+                  f"multiple times, supply the -f/--force option to " \
+                  f"`pybm create`."
+            raise GitError(msg)
 
         if not name:
             # TODO: Fallback if the designated directory exists
-            # worktree name repo@<sha> in the current directory
-            name = "@".join([self.repository_name, ref])
+            # worktree name repo@<ref> in the current directory
+            escaped = commit_ish.replace("/", "-")
+            name = "@".join([self.repository_name, escaped])
 
         return self.run_command("add", name, ref, force=force,
                                 checkout=checkout, lock=lock)
 
     def remove_worktree(self, info: str, force=False):
-        # find worktree by commit or tag
-        rm_worktree = self.get_worktree_by_attr(WORKTREE_ATTRS, info)
-        if not rm_worktree:
-            msg = f"fatal: Worktree with ID {info} does not exist."
+        # TODO: Improve info disambiguation - path match against repository
+        #  name, branch match local vs. remote, commit partial match
+        p = Path("./")
+        # All subdirectories in the current directory, not recursive.
+        all_subdirs = [f.stem for f in filter(Path.is_dir, p.iterdir())]
+        if info in all_subdirs:
+            wt, wt_id = self.get_worktree_by_name(info), "directory name"
+        elif info in list_local_branches():
+            wt, wt_id = self.get_worktree_by_branch(info), "branch name"
+        else:
+            wt, wt_id = self.get_worktree_by_commit(info), "commit"
+
+        if not wt:
+            msg = f"Worktree with {wt_id} {info} does not exist."
             raise GitError(msg)
 
         return self.run_command("remove", info, force=force)
--- pybm/git_utils.py
+++ pybm/git_utils.py
@@ -7,15 +7,6 @@ from pathlib import Path
 # small git flag database for worktree commands
 from pybm.exceptions import GitError
 
-_git_worktree_flags = {
-    "add": {"force": {True: "-f", False: None},
-            "checkout": {True: "--checkout", False: "--no-checkout"},
-            "lock": {True: "--lock", False: None},
-            },
-    "list": {"porcelain": {True: "--porcelain", False: None}},
-    "remove": {"force": {True: "-f", False: None}}
-}
-
 
 def lmap(fn, iterable: Iterable):
     return list(map(fn, iterable))
@@ -40,6 +31,41 @@ def is_git_repository():
 
 def get_repository_name():
     return Path.cwd().stem
+
+
+def is_valid_sha1_part(input_str: str, min_length: int = 7) -> bool:
+    # enforce a minimum length to help git resolve SHA1s, default is 7
+    if len(input_str) < min_length:
+        return False
+    else:
+        try:
+            # valid SHA1s can be cast to a hex integer
+            _ = int(input_str, 16)
+        except ValueError:
+            return False
+        return True
+
+
+def resolve_to_ref(commit_ish: str, resolve_commits: bool):
+    if commit_ish in list_tags():
+        # ref is a tag name
+        ref, ref_type = f"refs/tags/{commit_ish}", "tag"
+    # TODO: Make a worktree from a remote branch (does not appear here)
+    elif commit_ish in list_local_branches():
+        # ref is a local branch name
+        ref, ref_type = f"refs/heads/{commit_ish}", "branch"
+    elif is_valid_sha1_part(commit_ish):
+        ref, ref_type = commit_ish, "commit"
+    else:
+        msg = f"input {commit_ish} did not resolve to any known local " \
+              f"branch, tag or commit SHA1."
+        raise GitError(msg)
+
+    # force commit resolution, leads to detached HEAD
+    if resolve_commits:
+        ref, ref_type = resolve_commit(ref), "commit"
+
+    return ref, ref_type
 
 
 def list_tags():
@@ -68,15 +94,11 @@ def get_version() -> Tuple[int]:
         raise GitError(str(e))
 
 
-def resolve_commit(ref: str, ref_type: str = "branch") -> str:
-    if ref_type not in ["branch", "tag"]:
-        raise GitError(f"unknown ref type {ref_type} encountered in "
-                       f"git reference resolution attempt.")
-
-    resolved_ref = f"tags/{ref}" if ref_type == "tag" else f"refs/heads/{ref}"
+def resolve_commit(ref: str) -> str:
     try:
-        return subprocess.check_output(["git", "rev-list", "-n", "1",
-                                        resolved_ref]).decode("utf-8")
+        return subprocess.check_output(
+            ["git", "rev-list", "-n", "1", ref]
+        ).decode("utf-8")
     except subprocess.CalledProcessError as e:
         raise GitError(str(e))
 
@@ -89,20 +111,3 @@ def git_feature_guard(command_name: str, min_version: Tuple[int],
         raise GitError(f"The `git {command_name}` command was first added in "
                        f"git version {version_string(min_version)}, but your "
                        f"git version is {version_string(installed)}.")
-
-
-def parse_flags(command: str, **kwargs) -> List[str]:
-    flags = []
-    for k, v in kwargs.items():
-        command_options = _git_worktree_flags[command]
-        if k not in command_options:
-            # TODO: log bad kwarg usage somewhere
-            continue
-        cmd_opts = command_options[k]
-        if v not in cmd_opts:
-            raise ValueError(f"unknown value {v} given for option {k}.")
-        flag = cmd_opts[v]
-        if flag is not None:
-            flags.append(flag)
-
-    return flags
