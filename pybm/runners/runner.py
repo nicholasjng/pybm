3edffbd820d558254ef25d1c46ce9f38bfdc16de
Nicholas Junge <nicholas.junge@web.de>
2021-09-28 16:34:23 +0200

    Add first implementation of timeit benchmark runner
    This commit adds the first implementation of a timeit-based
    benchmark runner. The class API was improved to better take into
    account the challenge of standardizing, serializing and reparsing
    the flags inside of the subprocess.

--- pybm/runners/runner.py
+++ pybm/runners/runner.py
@@ -1,31 +1,125 @@
-from typing import Sequence, Optional, Tuple, Callable, List, Any
+import argparse
+from typing import Optional, Tuple, Callable, List, Any, Union, Dict
+from pathlib import Path
 
 from pybm import PybmConfig
 from pybm.exceptions import PybmError
 from pybm.mixins import SubprocessMixin
 from pybm.specs import BenchmarkEnvironment
-from pybm.util.common import lfilter
+from pybm.util.common import lfilter, lmap
 from pybm.util.functions import is_context_provider
 from pybm.util.imports import import_from_module
+from pybm.util.path import list_contents, get_subdirs
 
 # A context provider produces name and value of a contextual
 # piece of information, e.g. processor architecture, cwd etc.
 ContextProvider = Callable[[], Tuple[str, str]]
 
 
+def is_glob_expression(expr: Union[str, Path]) -> bool:
+    return "*" in str(expr)
+
+
 class BenchmarkRunner(SubprocessMixin):
     """Base class for all pybm benchmark runners."""
 
     def __init__(self, config: PybmConfig):
-        super().__init__(exception_type=PybmError)
-        self.required_packages: list[str] = config.get_value(
-            "runner.requiredPackages")
-        self.output_dir: str = config.get_value("runner.resultDirectory")
+        super().__init__()
+        self.prefix = "--benchmark"
+
+        # required packages for the runner
+        self.required_packages = []
+
+        self.result_dir: str = config.get_value("runner.resultDirectory")
         self.fail_fast: bool = config.get_value("runner.failFast")
         self.context_providers: List[ContextProvider] = \
             self.load_context_providers(config.get_value(
-                "runner.contextProviders")
-            )
+                "runner.contextProviders"))
+
+    def create_flags(
+            self,
+            result_file: Union[str, Path],
+            num_repetitions: int = 1,
+            benchmark_filter: Optional[str] = None,
+            benchmark_context: Optional[List[str]] = None) -> List[str]:
+        prefix = self.prefix
+        # JSON is the only supported output file format in GBM
+        flags = [f"{prefix}_out={result_file}",
+                 f"{prefix}_out_format=json"]
+        if benchmark_context is None:
+            benchmark_context = []
+        if benchmark_filter is not None:
+            flags.append(f"{prefix}_filter={benchmark_filter}")
+        if num_repetitions > 1:
+            flags.append(f"{prefix}_repetitions={num_repetitions}")
+        # Add benchmark context information like shown in
+        # https://github.com/google/benchmark/blob/main/docs/user_guide.md#extra-context
+        self.validate_context(benchmark_context)
+        flags += benchmark_context
+        return flags
+
+    def create_parser(self):
+        prefix = self.prefix
+        parser = argparse.ArgumentParser(prog=self.__class__.__name__,
+                                         add_help=False)
+        parser.add_argument(f"{prefix}_out",
+                            type=str,
+                            default=None)
+        parser.add_argument(f"{prefix}_out_format",
+                            type=str,
+                            choices=("json", "console"),
+                            default=None)
+        parser.add_argument(f"{prefix}_filter",
+                            type=str,
+                            default=None)
+        parser.add_argument(f"{prefix}_repetitions",
+                            type=int,
+                            default=1)
+        parser.add_argument(f"{prefix}_context",
+                            action="append",
+                            default=None)
+        return parser
+
+    def parse_flags(self, flags: List[str]):
+        parser = self.create_parser()
+        option_dict = vars(parser.parse_args(flags))
+        return option_dict
+
+    def create_result_dir(self, environment: BenchmarkEnvironment) -> Path:
+        subdirs = sorted(get_subdirs(self.result_dir))
+        ref, _ = environment.worktree.get_ref_and_type()
+        if not subdirs:
+            folder = "1"
+        else:
+            folder = str(int(subdirs[-1]) + 1)
+        result_path = Path(self.result_dir) / folder
+        # this method is called on dispatch, so the directory will only be
+        # created on the first target's dispatch call
+        result_path.mkdir(parents=True, exist_ok=True)
+        result_ref_dir = result_path / ref
+        assert not result_ref_dir.exists()
+        return result_ref_dir
+
+    def get_current_context(self) -> List[str]:
+        ctx_info = [
+            "--benchmark_context={0}={1}".format(*ctx()) for ctx in
+            self.context_providers]
+        return ctx_info
+
+    @staticmethod
+    def find_targets(path: Union[str, Path]):
+        benchmark_path = Path(path)
+        if benchmark_path.is_dir():
+            benchmark_targets = list_contents(benchmark_path,
+                                              file_suffix=".py")
+        elif benchmark_path.is_file():
+            benchmark_targets = list(str(path))
+        elif is_glob_expression(benchmark_path):
+            ppath, glob = benchmark_path.parent, benchmark_path.name
+            benchmark_targets = lmap(str, ppath.glob(glob))
+        else:
+            benchmark_targets = []
+        return benchmark_targets
 
     @staticmethod
     def load_context_providers(provider_info: str) -> List[Any]:
@@ -33,26 +127,52 @@ class BenchmarkRunner(SubprocessMixin):
             provider_names = provider_info.split(",")
         else:
             return []
+        # require the context providers to come from an installed module
         imported = [import_from_module(name) for name in provider_names]
         return lfilter(is_context_provider, imported)
 
-    def check_required_packages(self, environment: BenchmarkEnvironment) \
-            -> None:
+    def check_required_packages(self, environment: BenchmarkEnvironment):
         missing_pkgs = []
         for pkg in self.required_packages:
-            if pkg not in environment.venv.packages:
+            if pkg not in environment.get_value("python.packages"):
                 missing_pkgs.append(pkg)
         if len(missing_pkgs) > 0:
-            pkgs = " ".join(missing_pkgs)
-            raise PybmError(f"Required packages \"{pkgs}\" not installed "
-                            f"in environment {environment.name!r}. "
+            raise PybmError(f"Required packages {', '.join(missing_pkgs)} "
+                            f"for runner {self.__class__.__name__} not "
+                            f"installed in environment {environment.name!r}. "
                             f"To install them, run `pybm env install "
-                            f"{environment.name} {pkgs}`.")
+                            f"{environment.name} {' '.join(missing_pkgs)}`.")
 
-    def run_benchmarks(
+    @staticmethod
+    def validate_context(context: List[str]):
+        for ctx_val in context:
+            seen: Dict[str, str] = {}
+            split_ctx = ctx_val.split("=")
+            if len(split_ctx) != 3:
+                raise PybmError("Could not properly parse context value "
+                                f"{ctx_val!r}. Context values need to be "
+                                f"given as arguments in the format "
+                                f"\"--context=<key>=<value>\".")
+            name, value = split_ctx[1:]
+            if name in seen:
+                raise PybmError(f"Multiple values for context value "
+                                f"{name!r} were supplied. Perhaps you "
+                                f"gave some context information twice, "
+                                f"once on the command line as global context, "
+                                f"and via a set environment-specific context "
+                                f"provider. To check the currently set "
+                                f"environment-specific context providers, "
+                                f"run the command "
+                                f"`pybm config get runner.contextProviders`.")
+
+    def dispatch(
             self,
-            benchmarks: Sequence[str],
+            benchmarks: List[str],
             environment: BenchmarkEnvironment,
+            repetitions: int = 1,
             benchmark_filter: Optional[str] = None,
-            context_providers: Optional[Sequence[ContextProvider]] = None):
+            benchmark_context: Optional[List[str]] = None):
+        raise NotImplementedError
+
+    def run_benchmark(self, args: List[str] = None) -> int:
         raise NotImplementedError
--- pybm/runners/stdlib.py
+++ pybm/runners/stdlib.py
@@ -1,14 +1,28 @@
-from typing import List, Union, Optional, Sequence
+import functools
+import json
+import re
 import timeit
 from pathlib import Path
-import json
+from typing import List, Optional, Any, Tuple, Dict
 
 from pybm.config import PybmConfig
-from pybm.runners.runner import BenchmarkRunner, ContextProvider
+from pybm.runners.runner import BenchmarkRunner
 from pybm.specs import BenchmarkEnvironment
+from pybm.util.common import dfilter
+from pybm.util.functions import is_valid_timeit_target
 
 
-class StdlibBenchmarkRunner(BenchmarkRunner):
+def is_module_member(obj_tuple: Tuple[str, Any], module_name: str):
+    """Check membership of object in __main__ module to avoid benchmarking
+    imports due to accidentally pattern matching them."""
+    obj = obj_tuple[1]
+    if not hasattr(obj, "__module__"):
+        return False
+    else:
+        return obj.__module__ == module_name
+
+
+class TimeitBenchmarkRunner(BenchmarkRunner):
     """
     A benchmark runner class interface designed to dispatch benchmarking
     runs in pybm using Google Benchmark.
@@ -17,18 +31,70 @@ class StdlibBenchmarkRunner(BenchmarkRunner):
     def __init__(self, config: PybmConfig):
         super().__init__(config=config)
 
-    def run_benchmarks(
+    def dispatch(
             self,
-            benchmarks: Sequence[str],
+            benchmarks: List[str],
             environment: BenchmarkEnvironment,
+            repetitions: int = 1,
             benchmark_filter: Optional[str] = None,
-            context_providers: Optional[Sequence[ContextProvider]] = None):
+            benchmark_context: Optional[List[str]] = None):
+        result_dir = self.create_result_dir(environment=environment)
         # stupid name, only used for printing below
         n = len(benchmarks)
-        for i, target in enumerate(benchmarks):
-            ref, _ = environment.workspace.get_ref_and_type()
-            out_dir = Path(self.output_dir) / ref
-            out_dir.mkdir(parents=True, exist_ok=True)
-            out_name = Path(target).stem + "_results.json"
-            print(f"Running benchmark {target}.....[{i + 1}/{n}]")
+        print(f"Found a total of {n} benchmark targets for "
+              f"environment {environment.name!r}.")
+        for i, benchmark in enumerate(benchmarks):
+            print(f"Running benchmark {benchmark}.....[{i + 1}/{n}]")
+            python = environment.get_value("python.executable")
+            worktree_root = environment.get_value("worktree.root")
+            result_name = Path(benchmark).stem + "_results.json"
+            result_file = result_dir / result_name
+            command = [python, benchmark]
+            command += self.create_flags(
+                result_file=result_file,
+                num_repetitions=repetitions,
+                benchmark_filter=benchmark_filter,
+                benchmark_context=benchmark_context,
+            )
+            self.run_subprocess(command, cwd=worktree_root)
 
+    def run_benchmark(self, args: List[str] = None):
+        flag_parser = self.create_parser()
+        options = flag_parser.parse_args(args)
+        json_obj: Dict[str, Any] = {}
+        out_path = options.benchmark_out
+
+        # filter admissible targets as those from the __main__ module
+        # of the subprocess (target .py file)
+        is_main_member = functools.partial(is_module_member,
+                                           module_name="__main__")
+        module_targets = dfilter(is_main_member, globals())
+        # valid timeit target <=> is function + takes no args
+        module_targets = dfilter(is_valid_timeit_target, module_targets)
+        benchmark_filter = options.benchmark_filter
+        if benchmark_filter is not None:
+            pattern = re.compile(options.benchmark_filter)
+            module_targets = dfilter(
+                lambda x: pattern.match(x[0]) is not None, module_targets)
+
+        # construct and fill context dictionary object
+        benchmark_context: List[str] = options.benchmark_context
+        context_dict: Dict[str, str] = {}
+        for ctx in benchmark_context:
+            name, value = ctx.split("=")
+            context_dict[name] = value
+        # add to JSON object
+        json_obj["context"] = context_dict
+
+        num_reps = options.benchmark_repetitions
+        benchmarks = []
+        for name, func in module_targets.items():
+            benchmark_obj: Dict[str, Any] = {"name": name}
+            # Source: https://docs.python.org/3/library/timeit.html#examples
+            t = timeit.Timer(stmt=f"{name}()", globals=globals())
+            benchmark_obj["times"] = t.repeat(repeat=num_reps)
+            benchmarks.append(benchmark_obj)
+
+        json_obj["benchmarks"] = benchmarks
+        with open(out_path, "w") as result_file:
+            json.dump(json_obj, fp=result_file)
