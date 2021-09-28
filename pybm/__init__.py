5b82be70a5e498f39d69c1b520abf83505ada97b
Nicholas Junge <nicholas.junge@web.de>
2021-09-28 16:36:20 +0200

    Add pybm.run() shim for easy benchmark running in target file

--- pybm/__init__.py
+++ pybm/__init__.py
@@ -2,5 +2,13 @@ __version__ = "0.0.1"
 
 # Disables flake8 errors for unused imports (project structuring)
 # flake8: noqa: F401
+from typing import Optional, List
 
-from .config import PybmConfig
+from .config import PybmConfig, get_runner_class
+from .runners.runner import BenchmarkRunner
+
+
+def run(argv: Optional[List[str]] = None) -> int:
+    config_file = PybmConfig.load(".pybm/config.yaml")
+    runner: BenchmarkRunner = get_runner_class(config_file)
+    return runner.run_benchmark(argv)
