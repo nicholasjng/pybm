2c3c4f708e66fc7c2b0373e97e8162e0d3483163
Nicholas Junge <nicholas.junge@web.de>
2021-11-05 22:15:20 +0100

    Add dynamic option sourcing for result reporter class

--- pybm/reporters/base.py
+++ pybm/reporters/base.py
@@ -1,38 +1,41 @@
-from pybm import PybmConfig
 from pathlib import Path
 from typing import Union, Optional
+
+from pybm import PybmConfig
 
 
 class BenchmarkReporter:
     def __init__(self, config: PybmConfig):
-        self.result_dir: Path = Path(config.get_value(
-            "reporter.resultDirectory"))
-        self.target_time_unit: str = config.get_value(
-            "reporter.targetTimeUnit")
-        self.significant_digits: int = config.get_value(
-            "reporter.significantDigits"
-        )
+        self.result_dir: Path = Path(config.get_value("reporter.resultDirectory"))
+
+        self.target_time_unit: str = config.get_value("reporter.targetTimeUnit")
+
+        self.significant_digits: int = config.get_value("reporter.significantDigits")
 
     def add_arguments(self):
         raise NotImplementedError
 
-    def compare(self,
-                *refs: str,
-                result: Union[str, Path],
-                target_filter: Optional[str] = None,
-                benchmark_filter: Optional[str] = None,
-                context_filter: Optional[str] = None
-                ):
+    def compare(
+        self,
+        *refs: str,
+        result: Union[str, Path],
+        target_filter: Optional[str] = None,
+        benchmark_filter: Optional[str] = None,
+        context_filter: Optional[str] = None
+    ):
         raise NotImplementedError
 
-    def report(self,
-               ref: str,
-               result: Union[str, Path],
-               target_filter: Optional[str] = None,
-               benchmark_filter: Optional[str] = None,
-               context_filter: Optional[str] = None):
+    def report(
+        self,
+        ref: str,
+        result: Union[str, Path],
+        target_filter: Optional[str] = None,
+        benchmark_filter: Optional[str] = None,
+        context_filter: Optional[str] = None,
+    ):
         raise NotImplementedError
 
-    def load(self, ref: str, result: Union[str, Path],
-             target_filter: Optional[str] = None):
+    def load(
+        self, ref: str, result: Union[str, Path], target_filter: Optional[str] = None
+    ):
         raise NotImplementedError
