436d3613581c5aef9e3a8541a297dd531f0c57af
Nicholas Junge <nicholas.junge@web.de>
2022-04-26 17:51:27 +0200

    Provide functional logging to pybm
    This commit fixes the logging setup for `pybm`. A new default logstring
    was chosen that shows file name and line number, while not displaying
    timestamps anymore (as none of these commands are time critical per se).

--- pybm/logging.py
+++ pybm/logging.py
@@ -1,34 +1,18 @@
 import logging
 import pathlib
-import sys
 
-from pybm import PybmConfig, PybmError
-
-
-def get_file_handler(config: PybmConfig):
-    logfile = pathlib.Path(config.get_value("core.logfile"))
-    logfile.parent.mkdir(parents=False, exist_ok=True)
-    file_handler = logging.FileHandler(filename=logfile, mode="a+")
-    file_handler.setLevel(config.get_value("core.loglevel"))
-    file_handler.setFormatter(config.get_value("core.logfmt"))
-    return file_handler
-
-
-def get_stream_handler(config: PybmConfig):
-    stream_handler = logging.StreamHandler(stream=sys.stdout)
-    stream_handler.setFormatter(config.get_value("core.logfmt"))
-    stream_handler.setLevel(config.get_value("core.loglevel"))
-    return stream_handler
+from pybm.config import config
 
 
 def get_logger(name: str):
-    logger = logging.getLogger(name=name)
-    try:
-        config = PybmConfig.load()
-    except PybmError:
-        # run with default settings
-        config = PybmConfig()
+    level: int = config.get_value("core.loglevel")
+    fmt: str = config.get_value("core.logfmt")
+    filename = pathlib.Path(config.get_value("core.logfile"))
 
-    logger.setLevel(config.get_value("core.loglevel"))
-    logger.addHandler(get_file_handler(config=config))
+    # ensure the logfile directory actually exists
+    filename.parent.mkdir(parents=False, exist_ok=True)
+
+    logging.basicConfig(filename=filename, format=fmt, level=level)
+
+    logger = logging.getLogger(name=name)
     return logger
