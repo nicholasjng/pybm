5e2bf13b09d39f068f7d612fd980397884d88583
Nicholas Junge <nicholas.junge@web.de>
2023-08-07 20:08:59 +0200

    Simplify main entry point, include different error origins
    Gives a little more insight in the error messages on where the error
    actually occurred.

--- pybm/main.py
+++ pybm/main.py
@@ -3,22 +3,37 @@ import sys
 from typing import List, Optional
 
 from pybm.command import CLICommand
-from pybm.exceptions import CommandError, PybmError
-from pybm.parsers import parse_args, parse_command
+from pybm.commands import CommandMapping
+from pybm.exceptions import CommandError, GitError, PythonError
+from pybm.statuscodes import ERROR
+
+error_origins: dict[type[Exception], str] = {
+    GitError: "git",
+    PythonError: "python",
+}
+
+# CLI flag prefix for pybm
+prefix = "-"
 
 
 def main(args: Optional[List[str]] = None) -> int:
-    if not args:
-        # first argv is absolute script path
-        args = sys.argv[1:]
+    # first element of sys.argv is absolute script path
+    args = args or sys.argv[1:]
 
-    command_name, command_args = parse_args(args)
+    if not args or args[0].startswith(prefix):
+        command = ""
+    else:
+        command, *args = args
 
     try:
-        command: CLICommand = parse_command(command_name)
-    except (CommandError, PybmError) as e:
-        sys.stderr.write(f"Error: {e}")
-        sys.stderr.write(os.linesep)
-        sys.exit(1)
+        if command not in CommandMapping:
+            # TODO: Print similar commands if any, or print options
+            raise CommandError(f"Unknown command {command!r}")
 
-    return command.run_wrapped(command_args)
+        cmd: CLICommand = CommandMapping[command]()
+        return cmd.run_wrapped(args)
+    except Exception as e:
+        origin = error_origins.get(type(e), "pybm")
+        sys.stderr.write(f"{origin}: {e}")
+        sys.stderr.write(os.linesep)
+        return ERROR
